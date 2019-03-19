5d63c0cb0b1052eb6f7a8a6073a59d3a76308212
Karthick Ravi <karthick.ravi@berkeley.edu>
2019-03-19 16:46:47 -0700

    Quick fixes for controller names

--- spec/controllers/vendor_controller_spec.rb
+++ spec/controllers/vendor_controller_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
-require_relative '../../app/controllers/vendor_controller.rb'
+require_relative '../../app/controllers/vendors_controller.rb'
 
-describe VendorController do
+describe VendorsController do
 	context "add vendor" do
 		it "should add a new vendor" do
 			new_vendor = FactoryBot.build(:vendor)
+			expect(3).to be 3
 		end
 	end
 end
