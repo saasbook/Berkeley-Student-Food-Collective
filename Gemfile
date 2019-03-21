2f3bcaf9072504e73ca86483540215e04a20581e
Karthick Ravi <karthick.ravi@berkeley.edu>
2019-03-20 18:48:34 -0700

    Integrated Bootstrap and made progress on Cucumber:

--- Gemfile
+++ Gemfile
@@ -21,6 +21,9 @@ gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
 
+gem 'jquery-rails'
+
+
 gem 'bootstrap', '~> 4.3.1'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
--- features/step_definitions/vendor_steps.rb
+++ features/step_definitions/vendor_steps.rb
@@ -1,4 +1,4 @@
-And /I fill in the New Vendor form/ do 
+	And /I fill in the New Vendor form/ do 
 	steps %Q{
 		When I fill in "vendor_name" with "New Vendor Name"
 		When I fill in "vendor_address" with "Fake Address"
@@ -16,9 +16,21 @@ Then /the DB should be updated with the new vendor/ do
 end
 
 Then /the DB should not be updated with the new vendor/ do 
-	step %Q{
+	steps %Q{
 		Given I am on the All Vendors page
 		Then I should not see "Added Vendor: New Vendor Name to Database"
 	}
+end
+
+When /I click "cancel_button"/ do
+
+end
+
+Then /a confirm popup should appear/ do
+	#popup = page.driver.browser.switch_to.alert
+	accept_confirm do
+		click_button("cancel_button")
+	end
+	
 end
 
