92f0b81a669f23f146f177439ced83be4b6919eb
David Wang <david-wang@berkeley.edu>
2019-03-22 12:23:17 -0700

    Changed new/edit pages to use nested attributes and make controller actions simpler (no new tests yet)

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,17 +1,35 @@
-function confirmVendorCancel(){
-	confirmGeneralCancel("vendor")
+"use strict";
+
+function confirmVendorCancel() {
+	confirmGeneralCancel("vendor");
 }
 
-function confirmProductCancel(){
-	confirmGeneralCancel("product")
+function confirmProductCancel() {
+	confirmGeneralCancel("product");
 }
 
-function confirmGeneralCancel(input){
-    cancel = confirm("Are you sure you want to cancel creating this " + input + "? The information will not be saved!")
+function confirmGeneralCancel(input) {
+    var cancel = confirm("Are you sure you want to cancel creating this " + input + "? The information will not be saved!");
     if (cancel) {
-        alert("Canceling!")
-        window.location.replace("/" + input + "s")
+        alert("Canceling!");
+        window.location.replace("/" + input + "s");
     } else {
-        alert("Not Canceling!")
+        alert("Not Canceling!");
     }
+}
+
+function addNewTag() {
+    var newTagName = $("#new_tag_field").val();
+    var newTagNum = $(".tag").length;
+    var namePrefix = "vendor[tags_attributes][" + newTagNum + "]";
+    var idPrefix = "vendor_tags_attributes_" + newTagNum + "_";
+
+    var newDiv = $("<div>").attr({class: "tag"});
+
+    $("<label>").attr({for: idPrefix + "name"}).text("Name").appendTo(newDiv);
+    $("<input>").attr({type: "text", value: newTagName, name: namePrefix + "[name]", id: idPrefix + "name"}).appendTo(newDiv);
+    $("<input>").attr({type: "hidden", value: "0", name: namePrefix + "[_destroy]"}).appendTo(newDiv);
+    $("<input>").attr({type: "checkbox", value: "1", name: namePrefix + "[_destroy]", id: idPrefix + "_destroy"}).appendTo(newDiv);
+    $("<label>").attr({for: idPrefix + "_destroy"}).text("Remove Tag").appendTo(newDiv);
+    newDiv.appendTo("#tags");
 }
