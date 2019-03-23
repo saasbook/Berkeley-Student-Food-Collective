d1fa74090a9ed04a49e411211c2493cd3a10228c
David Wang <david-wang@berkeley.edu>
2019-03-23 01:30:09 -0700

    Added a cucumber test for vendor add (but broken)

--- features/add_vendor.feature
+++ features/add_vendor.feature
@@ -6,10 +6,17 @@ Feature: Add a new vendor to the database
 
   Scenario: Successfully add new vendor without tags (happy)
     Given I am on the New Vendor page
-    And I fill in the New Vendor form
-    When I press "Create Vendor"
+    When I fill in the New Vendor form
+    And I press "Create Vendor"
     Then the DB should be updated with the new vendor
 
+  Scenario: Successfully add new vendor with tags (happy)
+    Given I am on the New Vendor page
+    When I fill in the New Vendor form
+    And I add a new tag "a"
+    And I press "Create Vendor"
+    Then the DB should be updated with the new vendor
+    And the new vendor should have the tag "a"
 
 #  Scenario: successfully cancel a new vendor action (happy)
 #    Given I am on the New Vendor page
--- features/step_definitions/vendor_steps.rb
+++ features/step_definitions/vendor_steps.rb
@@ -6,42 +6,50 @@ Given /I fill in the New Vendor form/ do
   FactoryBot.attributes_for(:vendor).each do |key, value|
     step %Q{I fill in "vendor_#{key}" with "#{value}"}
   end
-end 
+end
+
+When /I add a new tag "(.*)"/ do |tag|
+  steps %Q{
+  	When I fill in "new_tag_field" with "#{tag}"
+		And I press "Add new tag"
+	}
+end
 
 Then /the DB should be updated with the new vendor/ do 
 	steps %Q{
 		Then I should be on the All Vendors page
-		Then I should see "Added Vendor: Default Vendor Name to Database"
+		And I should see "Added Vendor: Default Vendor Name to Database"
 	}
 end
 
-Then /the DB should not be updated with the new vendor/ do 
-	steps %Q{
-		Given I am on the All Vendors page
-		Then I should not see "Added Vendor: New Vendor Name to Database"
-	}
+Then /the new vendor should have the tag "(.*)"/ do |tag|
+  step %Q{I am on the Edit Vendor page}
+  find('div#tags').should have_content(tag)
 end
 
-When /I click "cancel_button"/ do
+# Then /the DB should not be updated with the new vendor/ do
+# 	steps %Q{
+# 		Given I am on the All Vendors page
+# 		Then I should not see "Added Vendor: New Vendor Name to Database"
+# 	}
+# end
 
-end
-
-Then /a confirm popup should appear/ do
-	#popup = page.driver.browser.switch_to.alert
-	accept_confirm do
-		click_button("cancel_button")
-	end
-end
+# Then /a confirm popup should appear/ do
+# 	#popup = page.driver.browser.switch_to.alert
+# 	accept_confirm do
+# 		click_button("cancel_button")
+# 	end
+# end
 
 Then /I should see the attributes, except tags, filled in/ do
 	vendor = Vendor.find_by_name(FactoryBot.attributes_for(:vendor)[:name])
   steps %Q{
   	Then the "vendor[name]" field should contain "#{vendor.name}"
-		Then the "vendor[description]" field should contain "#{vendor.description}"
-		Then the "vendor[address]" field should contain "#{vendor.address}"
-		Then the "vendor[facebook]" field should contain "#{vendor.facebook}"
-		Then the "vendor[twitter]" field should contain "#{vendor.twitter}"
-		Then the "vendor[instagram]" field should contain "#{vendor.instagram}"
+		And the "vendor[description]" field should contain "#{vendor.description}"
+		And the "vendor[address]" field should contain "#{vendor.address}"
+		And the "vendor[facebook]" field should contain "#{vendor.facebook}"
+		And the "vendor[twitter]" field should contain "#{vendor.twitter}"
+		And the "vendor[instagram]" field should contain "#{vendor.instagram}"
 	}
 end
 
