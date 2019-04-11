6fff57a2632acad35d6201b3e93378b976d2eb63
Karthick Ravi <karthick.ravi@berkeley.edu>
2019-04-10 22:31:37 -0700

    Fixed Code and Added Important Comments

--- app/assets/javascripts/carousel_scripts.js
+++ app/assets/javascripts/carousel_scripts.js
@@ -1,23 +1,30 @@
 $(document).ready(function() {
-	$(".trial_button").mouseenter(function() {
+	$(".carousel_element").mouseenter(function() {
 		current_index = $(this).index();
-		$(".trial_button").each(function(index){
+		$(".carousel_element").each(function(index){
 			console.log(index);
 			if (current_index < index) {
 				document.getElementById($(this).text()).classList.add("spreadLeft"); 
+				document.getElementById($(this).text()).classList.add("shade");
 			} else if (current_index > index) {
 				document.getElementById($(this).text()).classList.add("spreadRight"); 
+				document.getElementById($(this).text()).classList.add("shade");
+
 			}
 		});
 	});
 
-	$(".trial_button").mouseleave(function() {
+	$(".carousel_element").mouseleave(function() {
 		current_index = $(this).index();
-		$(".trial_button").each(function(index){
+		$(".carousel_element").each(function(index){
 			if (current_index < index) {
 				document.getElementById($(this).text()).classList.remove("spreadLeft"); 
+								document.getElementById($(this).text()).classList.remove("shade");
+
 			} else if (current_index > index) {
-				document.getElementById($(this).text()).classList.remove("spreadRight"); 
+				document.getElementById($(this).text()).classList.remove("spreadRight");
+								document.getElementById($(this).text()).classList.remove("shade");
+ 
 			}
 		});
 	});
