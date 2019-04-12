a62ec873cb507a6c28ee47cc96fde997c33b2b13
David Wang <dwang733@outlook.com>
2019-04-12 15:29:14 -0700

    Fix duplicate has_many through relationships in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,18 +11,7 @@ class Product < ActiveRecord::Base
   has_many :product_packagings
   has_many :packagings, through: :product_packagings
 
-  has_many :product_certifications
-	has_many :certifications, through: :product_certifications
-
   accepts_nested_attributes_for :certifications, :allow_destroy => true
-
-  has_many :product_nutritions
-	has_many :nutritions, through: :product_nutritions
-
   accepts_nested_attributes_for :nutritions, :allow_destroy => true
-
-  has_many :product_packagings
-	has_many :packagings, through: :product_packagings
-
   accepts_nested_attributes_for :packagings, :allow_destroy => true
 end
