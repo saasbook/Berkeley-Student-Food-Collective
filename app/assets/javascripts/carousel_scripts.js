a6bf57aa6e976f2ef385386ee6e5d00b8917be3d
David Wang <dwang733@outlook.com>
2019-04-16 16:09:34 -0700

    General minor refactoring, convert tabs to spaces, fix duplicated html ids in views

--- app/assets/javascripts/carousel_scripts.js
+++ app/assets/javascripts/carousel_scripts.js
@@ -1,37 +1,37 @@
 $(document).ready(function() {
   function classString(list) {
-		var returnString = ".";
-		for (i = 0; i < list.length; i++) {
-		  returnString += list[i] + ".";
-		}
-		return returnString;
+    let returnString = ".";
+    for (let i = 0; i < list.length; i++) {
+      returnString += list[i] + ".";
+    }
+    return returnString;
   }
   $(".carousel_element").mouseover(function() {
-		over_index = $(this).index();
-		let classesOver = classString(event.currentTarget.classList).substring(0, classString(event.currentTarget.classList).length - 1);
-		$(classesOver).each(function(index) {
-	  	element_index = $(this).index();
-	  	if (element_index < over_index) {
-	  		$(this).context.classList.add("spreadLeft"); 
-	  	} 
-	  	if (element_index > over_index) {
-				$(this).context.classList.add("spreadRight"); 
-	  	}
-	  });
-	});
-	$(".carousel_element").mouseleave(function() {
-	  leave_index = $(this).index();
-	  let classesLeave = classString(event.currentTarget.classList).substring(0, classString(event.currentTarget.classList).length - 1);
-	  $(classesLeave).each(function(index){
-		  i = $(this).index();
-		  if (i < leave_index) {
-				$(this).context.classList.remove("spreadLeft"); 
-		  } 
-		  if (i > leave_index) {
-				$(this).context.classList.remove("spreadRight"); 
-		  }
-	  });
-	});
+    let over_index = $(this).index();
+    let classesOver = classString(event.currentTarget.classList).substring(0, classString(event.currentTarget.classList).length - 1);
+    $(classesOver).each(function(index) {
+      let element_index = $(this).index();
+      if (element_index < over_index) {
+        $(this).context.classList.add("spreadLeft");
+      }
+      if (element_index > over_index) {
+        $(this).context.classList.add("spreadRight");
+      }
+    });
+  });
+  $(".carousel_element").mouseleave(function() {
+    leave_index = $(this).index();
+    let classesLeave = classString(event.currentTarget.classList).substring(0, classString(event.currentTarget.classList).length - 1);
+    $(classesLeave).each(function(index) {
+      i = $(this).index();
+      if (i < leave_index) {
+        $(this).context.classList.remove("spreadLeft");
+      }
+      if (i > leave_index) {
+        $(this).context.classList.remove("spreadRight");
+      }
+    });
+  });
 });
 
 function carouselAlert() {
--- features/carousel.feature
+++ features/carousel.feature
@@ -6,26 +6,26 @@ Feature: View and Interact with a Carousel of Products and Vendors
 
   @javascript
   Scenario: Hover Slide Interaction with Carousel (happy)
-  	Given I create three new vendors
-  	And I am on the Discovery page
-  	When I hover over the second carousel element
-		Then the left elements should shift to the left
-		And the right elements should shift to the right
+    Given I create three new vendors
+    And I am on the Discovery page
+    When I hover over the second carousel element
+    Then the left elements should shift to the left
+    And the right elements should shift to the right
 
-	@javascript
+  @javascript
   Scenario: Carousels for products should show up correctly (happy)
     Given I create three new products
     And I am on the Discovery page
-  	Then I should see a carousel for the product tag type "Original Packaging Name"
-		And I should see all products with the tag "Original Packaging Name"
-		And I should see a carousel for the product tag type "Original Nutrition Name"
-		And I should see all products with the tag "Original Nutrition Name"
-		And I should see a carousel for the product tag type "Original Certification Name"
-		And I should see all products with the tag "Original Certification Name"
+    Then I should see a carousel for the product tag type "Original Packaging Name"
+    And I should see all products with the tag "Original Packaging Name"
+    And I should see a carousel for the product tag type "Original Nutrition Name"
+    And I should see all products with the tag "Original Nutrition Name"
+    And I should see a carousel for the product tag type "Original Certification Name"
+    And I should see all products with the tag "Original Certification Name"
 
   @javascript
   Scenario: Carousels for vendors should show up correctly (happy)
     Given I create three new vendors
     And I am on the Discovery page
-  	And I should see a carousel for the ownership type "Original Ownership Name"
-		Then I should see all vendors with owned by "Original Ownership Name"
+    And I should see a carousel for the ownership type "Original Ownership Name"
+    Then I should see all vendors with owned by "Original Ownership Name"
