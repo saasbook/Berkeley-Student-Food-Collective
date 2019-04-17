3f29ca59caf45a057741df55af9d67e2083e837d
David Wang <dwang733@outlook.com>
2019-04-16 17:37:18 -0700

    Remove code duplication from carousel script

--- app/assets/javascripts/carousel_scripts.js
+++ app/assets/javascripts/carousel_scripts.js
@@ -6,32 +6,59 @@ $(document).ready(function() {
     }
     return returnString;
   }
-  $(".carousel_element").mouseover(function() {
-    let over_index = $(this).index();
+
+  function helperFunction(obj, apply) {
+    let over_index = obj.index();
     let classesOver = classString(event.currentTarget.classList).substring(0, classString(event.currentTarget.classList).length - 1);
     $(classesOver).each(function(index) {
-      let element_index = $(this).index();
-      if (element_index < over_index) {
-        $(this).context.classList.add("spreadLeft");
-      }
-      if (element_index > over_index) {
-        $(this).context.classList.add("spreadRight");
-      }
-    });
-  });
-  $(".carousel_element").mouseleave(function() {
-    leave_index = $(this).index();
-    let classesLeave = classString(event.currentTarget.classList).substring(0, classString(event.currentTarget.classList).length - 1);
-    $(classesLeave).each(function(index) {
-      i = $(this).index();
-      if (i < leave_index) {
-        $(this).context.classList.remove("spreadLeft");
-      }
-      if (i > leave_index) {
-        $(this).context.classList.remove("spreadRight");
+      index += 1;
+      if (index < over_index) {
+        apply.call($(this).context.classList, "spreadLeft");
+      } else if (index > over_index) {
+        apply.call($(this).context.classList, "spreadRight");
       }
     });
+  }
+
+  $(".carousel_element").mouseover(function() {
+    helperFunction($(this), DOMTokenList.prototype.add);
   });
+
+  $(".carousel_element").mouseout(function() {
+    helperFunction($(this), DOMTokenList.prototype.remove);
+  });
+
+  // $(".carousel_element").mouseover(function() {
+  //   console.log("over");
+  //   console.log($(this));
+  //   let over_index = $(this).index();
+  //   let classesOver = classString(event.currentTarget.classList).substring(0, classString(event.currentTarget.classList).length - 1);
+  //   $(classesOver).each(function(index) {
+  //     let element_index = index + 1;
+  //     if (element_index < over_index) {
+  //       $(this).context.classList.add("spreadLeft");
+  //     }
+  //     if (element_index > over_index) {
+  //       $(this).context.classList.add("spreadRight");
+  //     }
+  //   });
+  // });
+  //
+  // $(".carousel_element").mouseout(function() {
+  //   console.log("out");
+  //   console.log($(this));
+  //   leave_index = $(this).index();
+  //   let classesLeave = classString(event.currentTarget.classList).substring(0, classString(event.currentTarget.classList).length - 1);
+  //   $(classesLeave).each(function(index) {
+  //     let i = $(this).index();
+  //     if (i < leave_index) {
+  //       $(this).context.classList.remove("spreadLeft");
+  //     }
+  //     if (i > leave_index) {
+  //       $(this).context.classList.remove("spreadRight");
+  //     }
+  //   });
+  // });
 });
 
 function carouselAlert() {
