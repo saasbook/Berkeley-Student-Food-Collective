23533489310349347fd730067638674e20e99c9d
David Wang <dwang733@outlook.com>
2019-04-16 17:43:12 -0700

    Remove old carousel script code

--- app/assets/javascripts/carousel_scripts.js
+++ app/assets/javascripts/carousel_scripts.js
@@ -27,38 +27,6 @@ $(document).ready(function() {
   $(".carousel_element").mouseout(function() {
     helperFunction($(this), DOMTokenList.prototype.remove);
   });
-
-  // $(".carousel_element").mouseover(function() {
-  //   console.log("over");
-  //   console.log($(this));
-  //   let over_index = $(this).index();
-  //   let classesOver = classString(event.currentTarget.classList).substring(0, classString(event.currentTarget.classList).length - 1);
-  //   $(classesOver).each(function(index) {
-  //     let element_index = index + 1;
-  //     if (element_index < over_index) {
-  //       $(this).context.classList.add("spreadLeft");
-  //     }
-  //     if (element_index > over_index) {
-  //       $(this).context.classList.add("spreadRight");
-  //     }
-  //   });
-  // });
-  //
-  // $(".carousel_element").mouseout(function() {
-  //   console.log("out");
-  //   console.log($(this));
-  //   leave_index = $(this).index();
-  //   let classesLeave = classString(event.currentTarget.classList).substring(0, classString(event.currentTarget.classList).length - 1);
-  //   $(classesLeave).each(function(index) {
-  //     let i = $(this).index();
-  //     if (i < leave_index) {
-  //       $(this).context.classList.remove("spreadLeft");
-  //     }
-  //     if (i > leave_index) {
-  //       $(this).context.classList.remove("spreadRight");
-  //     }
-  //   });
-  // });
 });
 
 function carouselAlert() {
