69400083346cc7cfef5d5f60689e6443cf2aae39
Tiffany Perumpail <tperumpail@berkeley.edu>
2019-04-30 02:41:08 -0700

    Update products controller to account for Real Food Challenge tags

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,21 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+    
+    @product.tags = @product.certifications + @product.nutritions + @product.packagings
+    @product.rfc = []
+    if @product.lc_based
+    	@product.rfc.append(:lc_based)
+    end
+    if @product.fair
+    	@product.rfc.append(:fair)
+    end
+    if @product.eco_sound
+    	@product.rfc.append(:eco_sound)
+    end
+    if @product.humane
+    	@product.rfc.append(:humane)
+    end
   end
 
 end
