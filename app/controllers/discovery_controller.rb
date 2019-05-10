7c25a691e3deae630c73e56279abaa364139e76b
David Wang <dwang733@outlook.com>
2019-05-10 16:29:17 -0700

    Change discovery index page to use sensible tags_hash names, and start work on combining the two element partials

--- app/controllers/discovery_controller.rb
+++ app/controllers/discovery_controller.rb
@@ -10,7 +10,7 @@ class DiscoveryController < ApplicationController
     # We will need to iterate through the passed in list of products/vendors
     # to create carousel_element partials for each item (not sure how to create one partial for both products + vendors)
 
-    @ownerships_hash = Vendor.get_ownerships_hash
-    @tags_hash = Product.get_tags_hash
+    @vendors_tags_hash = Vendor.get_tags_hash
+    @products_tags_hash = Product.get_tags_hash
    end
 end
