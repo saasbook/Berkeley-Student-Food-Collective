64f8c0613301305816c77b15898288792f814b54
Natalie Leuenberger <nrleuenberger@gmail.com>
2019-05-13 21:01:30 -0700

    remove dead code

--- app/assets/javascripts/carousel_scripts.js
+++ app/assets/javascripts/carousel_scripts.js
@@ -6,27 +6,6 @@ $(document).ready(function() {
     }
     return returnString;
   }
-
-  function hoverHelper(obj, apply) {
-    let over_index = obj.index();
-    let classesOver = classString(event.currentTarget.classList).substring(0, classString(event.currentTarget.classList).length - 1);
-    $(classesOver).each(function(index) {
-      index += 1;
-      if (index < over_index) {
-        apply.call($(this).context.classList, "spreadLeft");
-      } else if (index > over_index) {
-        apply.call($(this).context.classList, "spreadRight");
-      }
-    });
-  }
-
-  $(".carousel_element").mouseover(function() {
-    hoverHelper($(this), DOMTokenList.prototype.add);
-  });
-
-  $(".carousel_element").mouseout(function() {
-    hoverHelper($(this), DOMTokenList.prototype.remove);
-  });
 });
 
 function carouselAlert() {
