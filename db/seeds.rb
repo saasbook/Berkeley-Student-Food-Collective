f164bf964b50965bf4db927bff49a0835bcddb82
Karthick Ravi <karthick.ravi@berkeley.edu>
2019-05-17 13:24:37 -0700

    Added Detail to SD

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,15 +26,17 @@ Certification.create(name: 'Non-GMO')
 Nutrition.create(name: 'Superfood')
 Nutrition.create(name: 'High in protein')
 
-=begin
+
+@lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+"
 
 @vendor_picture = "http://image.shutterstock.com/image-vector/vector-illustration-supermarket-grocery-store-260nw-518613133.jpg"
 
-@first_vendor = Vendor.create(name: 'First Vendor', picture: @vendor_picture, address: "", facebook: "", twitter: "", instagram: "", story: "", mission: "")
-@second_vendor = Vendor.create(name: 'Second Vendor', picture: @vendor_picture, address: "", facebook: "", twitter: "", instagram: "", story: "", mission: "")
-@third_vendor = Vendor.create(name: 'Third Vendor', picture: @vendor_picture, address: "", facebook: "", twitter: "", instagram: "", story: "", mission: "")
-@fourth_vendor = Vendor.create(name: 'Fourth Vendor', picture: @vendor_picture, address: "", facebook: "", twitter: "", instagram: "", story: "", mission: "")
-@fifth_vendor = Vendor.create(name: 'Fifth Vendor', picture: @vendor_picture, address: "", facebook: "", twitter: "", instagram: "", story: "", mission: "")
+@first_vendor = Vendor.create(name: 'First Vendor', picture: @vendor_picture, address: "", facebook: "https://www.facebook.com/DangFoods/?", twitter: "", instagram: "", story: @lorem, mission: @lorem)
+@second_vendor = Vendor.create(name: 'Second Vendor', picture: @vendor_picture, address: "", facebook: "https://www.facebook.com/DangFoods/?", twitter: "", instagram: "", story: @lorem, mission: @lorem)
+@third_vendor = Vendor.create(name: 'Third Vendor', picture: @vendor_picture, address: "", facebook: "https://www.facebook.com/DangFoods/?", twitter: "", instagram: "", story: @lorem, mission: @lorem)
+@fourth_vendor = Vendor.create(name: 'Fourth Vendor', picture: @vendor_picture, address: "", facebook: "https://www.facebook.com/DangFoods/?", twitter: "", instagram: "", story: @lorem, mission: @lorem)
+@fifth_vendor = Vendor.create(name: 'Fifth Vendor', picture: @vendor_picture, address: "", facebook: "https://www.facebook.com/DangFoods/?", twitter: "", instagram: "", story: @lorem, mission: @lorem)
 
 
 @first_vendor.ownerships << @poc
@@ -45,11 +47,11 @@ Nutrition.create(name: 'High in protein')
 
 @product_picture = "http://image.shutterstock.com/image-vector/cartoon-apple-260nw-651312034.jpg"
 
-@first_product = Product.create(name: 'First Product', vendor: @first_vendor, picture: @product_picture, vegan: "", gluten_free: "", dairy_free: "", lc_based: "", fair: "", eco_sound: "", humane: "", upc: "", origin: "", cultural_history: "")
-@second_product = Product.create(name: 'Second Product', vendor: @first_vendor, picture: @product_picture, vegan: "", gluten_free: "", dairy_free: "", lc_based: "", fair: "", eco_sound: "", humane: "", upc: "", origin: "", cultural_history: "")
-@third_product = Product.create(name: 'Third Product', vendor: @first_vendor, picture: @product_picture, vegan: "", gluten_free: "", dairy_free: "", lc_based: "", fair: "", eco_sound: "", humane: "", upc: "", origin: "", cultural_history: "")
-@fourth_product = Product.create(name: 'Fourth Product', vendor: @first_vendor, picture: @product_picture, vegan: "", gluten_free: "", dairy_free: "", lc_based: "", fair: "", eco_sound: "", humane: "", upc: "", origin: "", cultural_history: "")
-@fifth_product = Product.create(name: 'Fifth Product', vendor: @first_vendor, picture: @product_picture, vegan: "", gluten_free: "", dairy_free: "", lc_based: "", fair: "", eco_sound: "", humane: "", upc: "", origin: "", cultural_history: "")
+@first_product = Product.create(name: 'First Product', vendor: @first_vendor, picture: @product_picture, vegan: true, gluten_free: false, dairy_free: false, lc_based: false, fair: false, eco_sound: false, humane: false, upc: "", origin: "America", cultural_history: @lorem)
+@second_product = Product.create(name: 'Second Product', vendor: @first_vendor, picture: @product_picture, vegan: false, gluten_free: true, dairy_free: false, lc_based: false, fair: false, eco_sound: false, humane: false, upc: "", origin: "China", cultural_history: @lorem)
+@third_product = Product.create(name: 'Third Product', vendor: @first_vendor, picture: @product_picture, vegan: false, gluten_free: true, dairy_free: false, lc_based: false, fair: false, eco_sound: false, humane: false, upc: "", origin: "Mexico", cultural_history: @lorem)
+@fourth_product = Product.create(name: 'Fourth Product', vendor: @first_vendor, picture: @product_picture, vegan: false, gluten_free: true, dairy_free: false, lc_based: false, fair: false, eco_sound: false, humane: false, upc: "", origin: "Great Britain", cultural_history: @lorem)
+@fifth_product = Product.create(name: 'Fifth Product', vendor: @first_vendor, picture: @product_picture, vegan: false, gluten_free: true, dairy_free: false, lc_based: false, fair: false, eco_sound: false, humane: false, upc: "", origin: "India", cultural_history: @lorem)
 
 @first_product.certifications << @halal
 @second_product.certifications << @halal
@@ -57,7 +59,6 @@ Nutrition.create(name: 'High in protein')
 @fourth_product.certifications << @halal
 @fifth_product.certifications << @halal
 
-=end
 
 
 
