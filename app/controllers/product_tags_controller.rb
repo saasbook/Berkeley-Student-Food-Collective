8fb0c4aa90217a2cfc45bafada06914b8ebf73b1
SavvaVyatkin <savva.vyatkin@gmail.com>
2019-12-01 19:22:48 -0800

    adding new field to tags

--- app/controllers/product_tags_controller.rb
+++ app/controllers/product_tags_controller.rb
@@ -6,8 +6,11 @@ class ProductTagsController < ApplicationController
     def index
       render '/my_tags/index'
     end
+
     def new
-    	
-    end	
+    	# Make new product so form knows to make submit button say "Create Product"
+    	# Pass in params from form if redirected from #create
+    	@producer_tag = ProductTag.new(flash[:prev_params])
+  	end
   
   end
