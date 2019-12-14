0f7e7df9d80a9038f82460e3edb7c683763ca0df
Ikram Magzoub <ikram.magzoub@berkeley.edu>
2019-12-13 22:47:41 -0800

    please work

--- db/migrate/20191109011008_create_product_tag_categories.rb
+++ db/migrate/20191109011008_create_product_tag_categories.rb
@@ -3,7 +3,6 @@ class CreateProductTagCategories < ActiveRecord::Migration[5.2]
     create_table :product_tag_categories do |t|
       t.string :name
       t.string :description
-      t.string :picture
     end
   end
 end
