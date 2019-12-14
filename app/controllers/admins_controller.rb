f72030495c1364d3d7f657d0f0fcad3a0d80c385
asliakalin <asliakalin@berkeley.edu>
2019-12-13 17:58:51 -0800

    i hate travis

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,13 +2,11 @@ class AdminsController < ApplicationController
   before_action :set_admin, only: [:show, :edit, :update, :destroy]
 
   # GET /admins
-  # GET /admins.json
   def index
     @admins = Admin.all
   end
 
   # GET /admins/1
-  # GET /admins/1.json
   def show
   end
 
@@ -22,7 +20,6 @@ class AdminsController < ApplicationController
   end
 
   # POST /admins
-  # POST /admins.json
   def create
     @admin = Admin.new(admin_params)
 
@@ -38,7 +35,6 @@ class AdminsController < ApplicationController
   end
 
   # PATCH/PUT /admins/1
-  # PATCH/PUT /admins/1.json
   def update
     respond_to do |format|
       if @admin.update(admin_params)
@@ -52,7 +48,6 @@ class AdminsController < ApplicationController
   end
 
   # DELETE /admins/1
-  # DELETE /admins/1.json
   def destroy
     respond_to do |format|
       if current_admin != @admin
@@ -66,12 +61,10 @@ class AdminsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_admin
       @admin = Admin.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def admin_params
       params.require(:admin).permit(:name, :password, :password_confirmation)
     end
