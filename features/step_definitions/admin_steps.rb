ff071d64b14fc40b92774c62c3a7a72daf2daa2f
asliakalin <asliakalin@berkeley.edu>
2019-12-19 16:13:42 +0300

    reverting changes6

--- features/step_definitions/admin_steps.rb
+++ features/step_definitions/admin_steps.rb
@@ -30,3 +30,27 @@ When /^I create 3 tags with first category$/ do
     ProductTag.create(product_tag_category_id: 1, name: "Tag3")
     ProductTag.create(product_tag_category_id: 2, name: "Tag4")
 end
+
+
+When /^I create 5 products$/ do
+    ProductTagCategory.destroy_all
+    ProductTagCategory.create(name: "Tag Category", description: "First")
+    ProductTag.create(product_tag_category_id: 1, name: "Tag1")
+    MyVendor.destroy_all
+    MyVendor.create(name: 'Vendor1',
+                         picture: 'https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/health-benefits-of-apples-1296x728-feature.jpg?w=1155&h=1528',
+                         story: 'These are apples',
+                         facebook: 'https://www.facebook.com/',
+                         twitter: 'https://www.facebook.com/',
+                         instagram: 'https://www.facebook.com/')
+    p1 = MyProduct.create(my_vendor_id: 1, name: "P1", picture: "https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/health-benefits-of-apples-1296x728-feature.jpg?w=1155&h=1528", description: "These are apples")
+    p2 = MyProduct.create(my_vendor_id: 1, name: "P2", picture: "https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/health-benefits-of-apples-1296x728-feature.jpg?w=1155&h=1528", description: "These are apples")
+    p3 = MyProduct.create(my_vendor_id: 1, name: "P3", picture: "https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/health-benefits-of-apples-1296x728-feature.jpg?w=1155&h=1528", description: "These are apples")
+    p4 = MyProduct.create(my_vendor_id: 1, name: "P4", picture: "https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/health-benefits-of-apples-1296x728-feature.jpg?w=1155&h=1528", description: "These are apples")
+    p5 = MyProduct.create(my_vendor_id: 1, name: "P5", picture: "https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/health-benefits-of-apples-1296x728-feature.jpg?w=1155&h=1528", description: "These are apples")
+    p1.update_attributes(:tagslist => "Tag1")
+    p2.update_attributes(:tagslist => "Tag1")
+    p3.update_attributes(:tagslist => "Tag1")
+    p4.update_attributes(:tagslist => "Tag1")
+    p5.update_attributes(:tagslist => "Tag1")
+end
